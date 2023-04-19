95ec7a3e9440f1087aa573ec50cf6dd932a1b3ae
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2023-04-19 21:27:13 +0100

    initial commit (basically)

---
+++ components/example.tsx
@@ -0,0 +1,13 @@
+// Example from https://beta.reactjs.org/learn
+
+import { useState, useEffect } from 'react'
+import './example.module.css'
+
+function ExampleThing() {
+  return <div>Example</div>;
+
+}
+
+export default function Example() {
+  return <ExampleThing />;
+}
--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,20 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>RCN Docs</span>,
+  head: (
+      <>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:title" content="RCN Docs" />
+      </>
+  ),
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/RCNOverwatcher',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
+  docsRepositoryBase: 'https://github.com/RCNOverwatcher/docs/tree/main',
 }
 
 export default config
